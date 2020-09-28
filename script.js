9e16e8c76e489d023b17a3c387b9724eeb11363e
chyppoli <claude.hyppolite@gmail.com>
2020-09-28 17:41:05 -0400

    add variables for timer. add statement to change timer to 0 on page load

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Welcome to my world");
+var secondsLeft = 0;
+var timerEl = document.getElementById("countdown-timer");
+
+// change seconds left to 0 on page load
+timerEl.textContent="Time: " + secondsLeft;
